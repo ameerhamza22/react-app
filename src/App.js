ad6bef48675ed02952bfd17f270afd9b44e4f096
ameerhamza22 <40482709+ameerhamza22@users.noreply.github.com>
2020-07-26 14:40:43 +0500

    adding dinner and updating app.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import Dinner from './dinner'
 function App() {
   return (
-<div><h1>Hello World</h1>
-<h2>This is my First React App</h2>
-<h2>This is again calling</h2>
-<h2>This is my First React App</h2>
-<h2>This is my First React App</h2>
-<h1>
-hi
-</h1>
+<div>
+<Dinner /> 
 </div>
+
   );
 }
 
---
+++ src/dinner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Dinner()
+{
+  return  <h1>
+        Hey, Come Here Im serving you for the dinner
+    </h1>
+
+}
+export default Dinner;
