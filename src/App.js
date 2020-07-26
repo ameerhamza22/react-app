42ff9e234c4e1389aefb6afed353661d0dd79af4
ameerhamza22 <40482709+ameerhamza22@users.noreply.github.com>
2020-07-26 13:47:59 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
 <div><h1>Hello World</h1>
-<h2>This is my First React App</h2></div>
+<h2>This is my First React App</h2>
+<h2>This is my First React App</h2>
+</div>
   );
 }
 
